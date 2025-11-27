3ac37e8313d3d2388fb03538baa13c3dad810943
Alex Bucknall <alex.bucknall@gmail.com>
2025-11-27 13:11:36 +0000

    refactor: Update font handling in webpack configuration to use asset modules

--- webpack.config.ts
+++ webpack.config.ts
@@ -49,16 +49,11 @@ const commonConfig: Configuration = {
 		rules: [
 			{
 				test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: path.join('ui', 'fonts'),
-							publicPath: 'fonts',
-						},
-					},
-				],
+				type: 'asset/resource',
+				generator: {
+					filename: path.join('ui', 'fonts', '[name][ext]'),
+					publicPath: '../',
+				},
 			},
 			{
 				test: /\.svg$/,
