f82c460362c3eb9710b13752291605899364257e
Alex Bucknall <alex.bucknall@gmail.com>
2025-11-27 17:40:47 +0000

    fix: Refactor overlay icon component to improve styling and structure

--- src/ui/overlay-icon.tsx
+++ src/ui/overlay-icon.tsx
@@ -12,23 +12,33 @@ export class OverlayIcon extends React.PureComponent<OverlayIconProps> {
 		return (
 			<div
 				style={{
-					borderRadius: '50vh',
-					backgroundColor: '#3c3e42',
 					width: '100%',
 					height: '100vh',
-					fontSize: '14px',
-					userSelect: 'none',
-					cursor: 'pointer',
 					display: 'flex',
 					justifyContent: 'center',
 					alignItems: 'center',
-					color: '#d3d6db',
-					fontWeight: 600,
+					backgroundColor: 'transparent',
 				}}
-				onClick={this.props.onClick.bind(this)}
 			>
-				{this.props.icon}
-				{this.renderText()}
+				<div
+					style={{
+						borderRadius: '12px',
+						backgroundColor: '#3c3e42',
+						padding: '4px 8px',
+						fontSize: '14px',
+						userSelect: 'none',
+						cursor: 'pointer',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						color: '#d3d6db',
+						fontWeight: 600,
+					}}
+					onClick={this.props.onClick.bind(this)}
+				>
+					{this.props.icon}
+					{this.renderText()}
+				</div>
 			</div>
 		);
 	}
